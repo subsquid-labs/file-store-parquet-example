80c795f65fe1ec7b674a9d6901083b4c17371f24
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2024-04-11 11:20:11 +0200

    Removing lookupArchive()
    	modified: src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,5 +1,4 @@
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
-import {lookupArchive} from '@subsquid/archive-registry'
 import * as erc20abi from './abi/erc20'
 
 export const USDC_CONTRACT = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase()
@@ -8,7 +7,7 @@ export const processor = new EvmBatchProcessor()
 	.setBlockRange({
 		from: 6082465
 	})
-	.setGateway(lookupArchive('eth-mainnet'))
+	.setGateway('https://v2.archive.subsquid.io/network/ethereum-mainnet')
 	.addLog({
 		address: [USDC_CONTRACT],
 		topic0: [erc20abi.events.Transfer.topic]
